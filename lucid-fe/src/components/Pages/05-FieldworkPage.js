0828246f3c78fef50e4a6bffe6c64eab9acbb749
Carlo <c.jose.lucido@gmail.com>
2020-06-16 14:57:56 -0700

    made template file functional component structure in pages and added it to each skeleton page to guide faster work

---
+++ lucid-fe/src/components/Pages/05-FieldworkPage.js
@@ -0,0 +1,38 @@
+// import React, {useEffect} from "react";
+
+// import {connect} from "react-redux";
+
+// //ACTION FUNCTIONS
+// import{
+//     somefunction,
+//     someFailure
+// } from "../../actions";
+
+// //STYLE COMING FROM LESS CLASSES AND ANTD COMPONENTS
+
+// //PAGES
+// import {Route, Link} from "react-router-dom";
+
+// //THIS FUNCTIONAL COMPONENT
+
+// function FieldworkPage(props){
+//     useEffect(() => {
+//         props.somefunction()
+//     }, []);
+
+//     return(
+//         <div>
+//             <p>Some JSX</p>
+//         </div>
+        
+//     )
+// };
+
+
+
+// const mapDispatchToProps ={
+//     somefunction,
+//     someFailure
+// }
+
+// export default connect(state => state, mapDispatchToProps)(FieldworkPage);
