149b83b0b2ac5f1abdc76eb4855a216fe0929841
Carlo <c.jose.lucido@gmail.com>
2020-06-17 11:12:47 -0700

    removed unnecessary fixes for deployment from craco.config permenantly

--- lucid-fe/craco.config.js
+++ lucid-fe/craco.config.js
@@ -1,12 +1,8 @@
 const CracoLessPlugin = require('craco-less');
-// const path = require('path')
+
 
 module.exports = {
-    // webpack:{
-    //     alias:{
-    //         '@': path.resolve(__dirname, 'src/')
-    //     }
-    // },
+
     plugins: [
         {
             plugin: CracoLessPlugin,
