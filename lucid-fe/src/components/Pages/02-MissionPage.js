9fb7fa836b4ff6c4ce8d4e37322338ae04c04ab4
Carlo <c.jose.lucido@gmail.com>
2020-06-20 15:54:36 -0700

    removed the word image from alts on mission page for netlify's optimization errors

--- lucid-fe/src/components/Pages/02-MissionPage.js
+++ lucid-fe/src/components/Pages/02-MissionPage.js
@@ -37,9 +37,9 @@ function MissionPage(props){
                     <h1>Your <span style={{color: "#2A1CEC"}}>Future</span> <span style={{fontSize:"1.3em"}}>+</span> Our <span style={{color: "#0CB906"}}>Past</span> <br/> <span style={{color:"#E88015"}}>Now</span>.</h1>
                     </div>   
                     <div class="flex-box-mission2 s2-b-images">
-                    <img src={drone} alt="top down image of drone"/>
-                    <img src={navigation} alt="icon image of map with location marker"/>
-                    <img src={trowel} style={{paddingLeft:"15em", marginTop:"2em"}} alt="icon image of a trowel, a tool used by archaeologists during excavation"/>
+                    <img src={drone} alt="top down icon of drone"/>
+                    <img src={navigation} alt="icon of map with location marker"/>
+                    <img src={trowel} style={{paddingLeft:"15em", marginTop:"2em"}} alt="icon of a trowel, a tool used by archaeologists during excavation"/>
                     </div>
                     <div class="grid-box s2-c-mission-text">
                         <p class="top">We've all heard the adage that those who ignore history are doomed to repeat it, Lucid Consulting helps end that pattern. Owned by technology driving anthropologists, we are intimate with applying historical information and broader cultural context to our problem solving approach.</p> 
